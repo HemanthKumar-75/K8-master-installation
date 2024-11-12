7fa28d0b8be00fedf0339201517d44dc221897e1
hemanth_kumar <hemanth4125@gmail.com>
2024-11-12 13:21:44 +0530

    K8 setup

--- scripts.sh
+++ scripts.sh
@@ -35,8 +35,8 @@ mv /tmp/eksctl /usr/local/bin
 
 # aws configure
 public_IP=$( curl ifconfig.me )
-scp ~/.aws/credentials user@$public_IP:~/.aws/credentials
-scp ~/.aws/config user@$public_IP:~/.aws/config
+scp -r C:\Users\hemanth\.aws\credentials ec2-user@$public_IP:~/.aws/credentials
+scp -r C:\Users\hemanth\.aws\config ec2-user@$public_IP:~/.aws/config
 chmod 600 ~/.aws/credentials
 chmod 600 ~/.aws/config
 
