7b5e50caa5f40645e5327cf5d19ae9a5b272fab1
hemanth_kumar <hemanth4125@gmail.com>
2024-11-18 15:57:25 +0530

    K8 setup

--- scripts.sh
+++ scripts.sh
@@ -43,3 +43,10 @@ git clone https://github.com/HemanthKumar-75/K8-expense.git
 git clone https://github.com/HemanthKumar-75/K8-eksctl.git
 cd K8-eksctl
 # /usr/local/bin/eksctl eksctl create cluster --config-file=eksctl.yaml
+
+# ebs-csi-drivers kubectl installations
+kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.37"
+
+# efs-csi-drivers kubectl installations
+kubectl kustomize \
+    "github.com/kubernetes-sigs/aws-efs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-2.1" > public-ecr-driver.yaml
